2db44e300d2db5b8c4f65679770cfa49ff38ddbe
Alexander Weinert <weinert@react.uni-saarland.de>
2016-06-02 10:37:27 +0200

    Code review of configuration system

--- src/main/scala/com/automatatutor/lib/Config.scala
+++ src/main/scala/com/automatatutor/lib/Config.scala
@@ -6,47 +6,49 @@ import net.liftweb.common.Full
 import net.liftweb.util.Props
 
 object Config {
+
+  abstract class ConfigParam[T]() { def get: T }
+
+  private case class StringParam(protected val accessor: Accessor) extends ConfigParam[String]() { def get = accessor.access }
+  private case class IntParam(protected val accessor: Accessor) extends ConfigParam[Int]() { def get = accessor.access.toInt }
+
+  private abstract class Accessor { def access: String }
+  private case class Prop(id: String, fallback: Accessor) extends Accessor {
+    def access = Props.get(id) match {
+      case Full(prop) => prop
+      case Empty      => fallback.access
+      case _          => fallback.access
+    }
+  }
+  private case class Default(definition: String) extends Accessor { def access = definition }
+  private case object Mandatory extends Accessor { def access = throw new Exception("Improper configuration") }
+
   object layout {
-    object usersPerPage extends IntParam(Prop("layout.usersperpage", Default("50")))
+    def usersPerPage: ConfigParam[Int] = IntParam(Prop("layout.usersperpage", Default("50")))
   }
-  
+
   object mail {
-    object user extends StringParam(Prop("mail.user", Default("")))
-    object password extends StringParam(Prop("mail.password", Default("")))
-    object from extends StringParam(Prop("mail.from", Default("noreply@automatatutor.com")))
+    def user: ConfigParam[String] = StringParam(Prop("mail.user", Default("")))
+    def password: ConfigParam[String] = StringParam(Prop("mail.password", Default("")))
+    def from: ConfigParam[String] = StringParam(Prop("mail.from", Default("noreply@automatatutor.com")))
   }
-  
+
   object db {
-    object driver extends StringParam(Prop("db.driver", Mandatory))
-    object url extends StringParam(Prop("db.url", Mandatory))
-    object user extends ConfigParam[Box[String]] { def get = Props.get("db.user") }
-    object password extends ConfigParam[Box[String]] { def get = Props.get("db.password") }
+    def driver: ConfigParam[String] = StringParam(Prop("db.driver", Mandatory))
+    def url: ConfigParam[String] = StringParam(Prop("db.url", Mandatory))
+    def user: ConfigParam[Box[String]] = new ConfigParam[Box[String]] { def get = Props.get("db.user") }
+    def password: ConfigParam[Box[String]] = new ConfigParam[Box[String]] { def get = Props.get("db.password") }
   }
-  
+
   object admin {
-    object firstname extends StringParam(Prop("admin.firstname", Default("Admin")))
-    object lastname extends StringParam(Prop("admin.lastname", Default("Admin")))
-    object password extends StringParam(Prop("admin.password", Default("admin")))
-    object email extends StringParam(Prop("admin.email", Default("admin@automatatutor.com")))
+    def firstname: ConfigParam[String] = StringParam(Prop("admin.firstname", Default("Admin")))
+    def lastname: ConfigParam[String] = StringParam(Prop("admin.lastname", Default("Admin")))
+    def password: ConfigParam[String] = StringParam(Prop("admin.password", Default("admin")))
+    def email: ConfigParam[String] = StringParam(Prop("admin.email", Default("admin@automatatutor.com")))
   }
-  
+
   object grader {
-    object url extends StringParam(Prop("grader.url", Mandatory))
-    object methodnamespace extends StringParam(Prop("grader.methodnamespace", Mandatory))
+    def url: ConfigParam[String] = StringParam(Prop("grader.url", Mandatory))
+    def methodnamespace: ConfigParam[String] = StringParam(Prop("grader.methodnamespace", Mandatory))
   }
 }
-
-abstract class ConfigParam[T]() { def get : T }
-case class StringParam(protected val accessor : Accessor) extends ConfigParam[String]() { def get = accessor.access }
-case class IntParam(protected val accessor : Accessor) extends ConfigParam[Int]() { def get =  accessor.access.toInt }
-
-abstract class Accessor { def access : String }
-case class Prop(id : String, fallback : Accessor) extends Accessor { 
-  def access = Props.get(id) match { 
-    case Full(prop) => prop
-    case Empty => fallback.access
-    case _ => fallback.access
-  }
-}
-case class Default(definition : String) extends Accessor { def access = definition }
-case object Mandatory extends Accessor { def access = throw new Exception("Improper configuration") }
