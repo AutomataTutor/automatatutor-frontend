ceafcf35b8f4ccc1198cd0caa4cadb24111af35b
Alexander Weinert <weinert@react.uni-saarland.de>
2016-09-04 19:26:51 +0200

    Fix bug when showing NFAToDFA-solve widget
    When drawing two applets for solving a NFA to DFA problem, the same
    configuration was passed to both applets, resulting in the configuration
    of the latter overriding that of the former.
    
    We now create a fresh copy of the configuration for each applet, which fixes
    this bug.

--- src/main/webapp/javascript/includeDFANFA.js
+++ src/main/webapp/javascript/includeDFANFA.js
@@ -1,15 +1,18 @@
 var Editor = {
-  curConfig: {
+  curConfigNfa: {
+    dimensions: [740,480]
+  },
+  curConfigDfa: {
     dimensions: [740,480]
   }
 };
 
 function initCanvas() {
   if(!Editor.canvasNfa) {
-      Editor.canvasNfa = new $.SvgCanvas("#svgcanvasnfa", Editor.curConfig, 'nondetaut'); 
+      Editor.canvasNfa = new $.SvgCanvas("#svgcanvasnfa", Editor.curConfigNfa, 'nondetaut'); 
   }
   if(!Editor.canvasDfa) {
-      Editor.canvasDfa = new $.SvgCanvas("#svgcanvasdfa", Editor.curConfig, 'detaut'); 
+      Editor.canvasDfa = new $.SvgCanvas("#svgcanvasdfa", Editor.curConfigDfa, 'detaut'); 
   }
 }
 
